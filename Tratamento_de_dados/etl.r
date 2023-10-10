cb8d5bf4de0e7e9e3417fbecc1c9080ebe649960
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-10-09 21:55:20 -0300

    Adiciona criação inicial de arquivo JSON

--- Tratamento_de_dados/etl.r
+++ Tratamento_de_dados/etl.r
@@ -1,34 +1,37 @@
 # Criando um ETL básico, cujo objetivo é ler, transformar os dados CSV e carregar estes dados em formato JSON.
 # Todo dado que for NA converter em null
 # Os dados devem ser organizados por objetos no JSON, separes por ESTADO
-library("jsonlite") 
 
+# PACKAGES
+library("jsonlite")  # jsonlite, pacote de manipulação de JSON
+library("dplyr") # dplyer pacote de manipulação de dados para o R
+library("stringr") # stringr, pacote que adiciona metodos para manipular strings
+
+# LEITURA DE ARQUIVO CSV
 dadosCsv = read.csv("Churn.csv", sep = ";", na.strings = "")
 
 colnames(dadosCsv) = c("Id", "Score", "Estado", "Genero", "Idade",
 "Patrimonio", "Saldo", "Produtos", "TemCartCredito", "Ativo", "Salario", "Saiu")
 
 # REMOVENDO ESTADOS DUPLICADOS
-#install.packages('dplyr')
-library("dplyr") # dplyer pacote de manipulação de dados para o R
-
 Estados = dadosCsv['Estado'] %>% distinct() #  Pipe operator: %>%
-View(Estados) # RS, SC, PR e etc...
 
-# TRANSFORMANDO EM JSON
 
-# colNames = colnames(dadosCsv)
-# jsonText = ""
+# CONSTRUINDO STRING JSON
+jsonText = '{'
+lastEl = Estados$Estado[length(Estados$Estado)]
 
-# for (col in colNames) {
-#     jsonText = paste(jsonText, '{
+for (col in Estados$Estado) { #col duplicado aqui
+    jsonText = paste(jsonText, str_interp('"${col}": []'))
+    
+    if(col != lastEl) {
+        jsonText = paste(jsonText, ',')
+    }
+}
 
-#     }')
+jsonText = paste(jsonText, '}')
 
-#     #print(dadosCsv[col])
-# }
-
-# print(jsonText)
-
+# CRIANDO ARQUIVO .JSON
+write(toJSON(fromJSON(jsonText)), "myJSON.json") 
 
 
