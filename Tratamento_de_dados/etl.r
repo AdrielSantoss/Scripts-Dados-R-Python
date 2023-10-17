a081d82ff173836c4b8c74b4161c7c2a8ddd0943
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-10-17 01:01:15 -0300

    Adiciona objetos de dados JSON dentro dos agrupadores.

--- Tratamento_de_dados/etl.r
+++ Tratamento_de_dados/etl.r
@@ -8,30 +8,56 @@ library("dplyr") # dplyer pacote de manipulação de dados para o R
 library("stringr") # stringr, pacote que adiciona metodos para manipular strings
 
 # LEITURA DE ARQUIVO CSV
-dadosCsv = read.csv("Churn.csv", sep = ";", na.strings = "")
+dados_csv <- read.csv("Churn.csv", sep = ";", na.strings = "")
 
-colnames(dadosCsv) = c("Id", "Score", "Estado", "Genero", "Idade",
-"Patrimonio", "Saldo", "Produtos", "TemCartCredito", "Ativo", "Salario", "Saiu")
+colnames(dados_csv) <- c("Id", "Score", "Estado", "Genero", "Idade",
+                         "Patrimonio", "Saldo", "Produtos", "TemCartCredito",
+                         "Ativo", "Salario", "Saiu")
+
+col_names <- colnames(dados_csv) # LENDO OS NOMES DAS COLUNAS
 
 # REMOVENDO ESTADOS DUPLICADOS
-Estados = dadosCsv['Estado'] %>% distinct() #  Pipe operator: %>%
-
+Estados <- dados_csv['Estado'] %>% distinct() #  Pipe operator: %>%
+Estados[is.na(Estados)] <- "Not grouped"
 
 # CONSTRUINDO STRING JSON
-jsonText = '{'
-lastEl = Estados$Estado[length(Estados$Estado)]
+json_content <- "{"
 
-for (col in Estados$Estado) {
-    jsonText = paste(jsonText, str_interp('"${col}": []'))
-    
-    if(col != lastEl) {
-        jsonText = paste(jsonText, ',')
+last_element <- Estados$Estado[length(Estados$Estado)]
+
+for (grouper in Estados$Estado) {
+  json_content <- paste(json_content, str_interp('"${grouper}": ['))
+  infos <- dados_csv[dados_csv$Estado == grouper, ]
+
+  for (row in 1:nrow(infos)) {
+    json_content <- paste(json_content, "{")
+
+    for (key in col_names) {
+      value <- infos[row, ][key]
+      json_content <- paste(json_content, str_interp('"${key}": "${value}"'))
+
+      last_key_name <- col_names[length(col_names)]
+
+      if (key != last_key_name) {
+        json_content <- paste(json_content, ",")
+      }
     }
+
+    json_content <- paste( json_content, "}")
+
+    if (row != nrow(infos)) {
+      json_content <- paste(json_content, ",")
+    }
+  }
+
+  json_content <- paste(json_content, "]")
+
+  if (grouper != last_element) {
+    json_content <- paste(json_content, ",")
+  }
 }
 
-jsonText = paste(jsonText, '}')
+json_content <- paste(json_content, "}")
 
 # CRIANDO ARQUIVO .JSON
-write(toJSON(fromJSON(jsonText)), "myJSON.json") 
-
-
+write(toJSON(fromJSON(json_content)), "myJSON.json")
