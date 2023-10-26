bfb413fc4003428684c684a6a05e5b3ba78e5375
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-10-26 15:56:37 -0300

    Adiciona novos exemplos com Numpy em introduction.py

--- Python/introduction.py
+++ Python/introduction.py
@@ -32,19 +32,57 @@ Precos = {"lapis": 5.5, "borracha": 7.0, "caneta": 6.5}
 # Sets, conjuntos não ordenados e não repetidos de elementos
 animais = {'gato', 'cachorro'}
 
-# Numpy
-a = np.array([1, 12, 23, 27])
-print(a.dtype)
-
-# Pandas
-dados = pd.read_csv('file.csv')
-
-# Funções
-
-
+# Functions
 def imprime(n=1):  # Função com parametros com valroes Default
     print("esta é uma função")
     return n * n
 
-
 imprime(3)
+
+# Exemplos com Numpy
+
+# Cria uma matriz, unidimensional
+import numpy as np
+mt = np.array([12, 34, 76])
+print(mt)
+print(type(mt))
+
+# Cria o array com um tipo especifico
+# Cria array com float de 64 bits
+import numpy as np
+mtfloat = np.array([1, 2, 3.4], dtype=np.float64)
+print(mtfloat)
+mtnew = mtfloat.astype(np.int32) # Alterando tipos de arrays
+print(mtnew)
+
+# Cria uma matriz bidimensional
+import numpy as np
+mt7 = np.array([[1,2,3], [4,5,6], [7, 8, 9]])
+print(mt7)
+
+# Cria não inicializados, array com valores zerados (0)
+import numpy as np
+vazio = np.empty([3,2], dtype=int) # Array 3 linhas e 2 colunas
+print(vazio)
+
+# Cria uma matriz 4x3 com valores zeros
+import numpy as np
+zeros = np.zeros([4,3], dtype=int)
+print(zeros)
+
+# Cria uma matriz 5x7 com valores igual a um
+import numpy as np
+ones = np.ones([5,7], dtype=int)
+print(ones)
+
+# Cria uma matriz quandrada com diagonal principal com valores 1 e outros valores zero
+import numpy as np
+diagonal = np.eye(5) # 5 linhas e 5 colunas
+print(diagonal)
+
+# Valores aleatórios entre zero e um
+import numpy as np
+ale = np.random.random((1))
+print(ale)
+
+# Exemplos com Pandas
